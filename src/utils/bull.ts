0b0df018929f576453376c31e3ff3175cf82b7f9
olsommer <oliver.os.sommer@gmail.com>
2023-10-15 14:25:23 +0200

    redis connection

--- src/utils/bull.ts
+++ src/utils/bull.ts
@@ -1,7 +1,10 @@
 import { Queue } from "bullmq";
 export const queue = new Queue("introlines", {
   connection: {
-    host: process.env.REDIS_URL,
+    host: "ec2-52-51-176-162.eu-west-1.compute.amazonaws.com",
+    password:
+      "peb687183a55fafff3be0835c38df7e6f3a961831ee445bd73d79e518040a2c68",
+    port: 19090,
   },
   // redis: process.env.REDIS_URL,
   // limiter: {
