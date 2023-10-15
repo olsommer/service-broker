512eb6f9bff4466db1626da930eb0530e56ed8d3
olsommer <oliver.os.sommer@gmail.com>
2023-10-15 15:08:47 +0200

    maxRetriesPerRequest

--- src/utils/bull.ts
+++ src/utils/bull.ts
@@ -2,7 +2,10 @@ import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
 // Redis connection details
-export const connection = new IORedis(process.env.REDIS_URL ?? "");
+// https://github.com/redis/ioredis/blob/v4/API.md
+export const connection = new IORedis(process.env.REDIS_URL ?? "", {
+  maxRetriesPerRequest: 0,
+});
 
 export const queue = new Queue("introlines", {
   connection,
