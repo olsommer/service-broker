8a829fa07bf38d57e164bb2eea89a35e49b56778
olsommer <oliver.os.sommer@gmail.com>
2023-10-17 01:15:02 +0200

    fix

---
+++ src/tasks/scrape/convertToPlain4.ts
@@ -0,0 +1,37 @@
+import { log } from "../log";
+import cheerio from "cheerio";
+
+export async function convertToPlain(html: string) {
+  const $ = cheerio.load(html);
+  const container = $("body");
+
+  if (!container) {
+    throw new Error("Could not parse HTML");
+  }
+  // Extract raw text content from the container element and its children
+  const rawTextContent = container.text();
+  await log(
+    "OK",
+    rawTextContent,
+    "35b9c23b-6d90-4359-ac2b-662d0efc6186",
+    "scrape",
+  );
+
+  // const cleanedText = rawTextContent.replace(/\s+/g, " ");
+  // The regular expression [\n\r\t]+ matches one or more line breaks
+  // (including newline \n and carriage return \r) and tabs \t, and replaces them with a single space.
+  // Afterward, the regular expression /\s+/g is used to replace multiple consecutive
+  // spaces with a single space. This should give you a cleaned text with
+  // line breaks removed and spaces between words preserved.
+  const cleanedText = rawTextContent
+    .replace(/[\n\r\t]+/g, " ")
+    .replace(/\s+/g, " ")
+    .replace(/"/g, " ") // Escape double quotes
+    .replace(/{/g, " ") // Escape curly braces {
+    .replace(/}/g, " ") // Escape curly braces }
+    .replace(/\\/g, " ") // Escape backslashes
+    .replace(/[\x00-\x1F]/g, " ");
+
+  const trimmedText = cleanedText.trim();
+  return trimmedText;
+}
