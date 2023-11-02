6dba41751f20f257a655d6d443c2e0afed4daf9d
olsommer <oliver.os.sommer@gmail.com>
2023-11-02 23:26:22 +0100

    fix

--- src/tasks/scrape/index.ts
+++ src/tasks/scrape/index.ts
@@ -104,6 +104,13 @@ export async function scrape(job: SandboxedJob<Payload, any>) {
       throw new Error("Content is empty");
     }
 
+    await log(
+      "OK",
+      content,
+      id,
+      "New Scraper test",
+    );
+
     /* Clean */
     const content_cleaned = await convertToPlain(content);
 
